8b11733965c18f9614fb70d90fcba04b5616318f
sonnyjon <sonny.s.jon@gmail.com>
2022-05-18 15:31:55 -0700

    Refactored service interfaces to a common base interface (i.e. CrudService). Closes #10

--- pet-clinic-data/src/main/java/dev/sonnyjon/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/dev/sonnyjon/sfgpetclinic/services/VetService.java
@@ -2,13 +2,6 @@ package dev.sonnyjon.sfgpetclinic.services;
 
 import dev.sonnyjon.sfgpetclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long> {
 
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 }
