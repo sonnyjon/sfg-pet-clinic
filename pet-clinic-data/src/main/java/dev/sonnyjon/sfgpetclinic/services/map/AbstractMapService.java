73d5c25d7829aedea15493644895c6259ae37da5
sonnyjon <sonny.s.jon@gmail.com>
2022-05-23 17:19:32 -0700

    Issue #22: Refactor and Auto-generate Map IDs

--- pet-clinic-data/src/main/java/dev/sonnyjon/sfgpetclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/dev/sonnyjon/sfgpetclinic/services/map/AbstractMapService.java
@@ -1,13 +1,12 @@
 package dev.sonnyjon.sfgpetclinic.services.map;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import dev.sonnyjon.sfgpetclinic.model.BaseEntity;
 
-public abstract class AbstractMapService<T, ID> {
+import java.util.*;
 
-    protected Map<ID, T> map = new HashMap<>();
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
+
+    protected Map<Long, T> map = new HashMap<>();
 
     Set<T> findAll() {
         return new HashSet<>(this.map.values());
@@ -17,9 +16,19 @@ public abstract class AbstractMapService<T, ID> {
         return this.map.get(id);
     }
 
-    T save(ID id, T object) {
+    T save(T object) {
 
-        this.map.put(id, object);
+        if (object != null) {
+            if (object.getId() == null) {
+                object.setId(getNextId());
+            }
+
+            this.map.put(object.getId(), object);
+        }
+        else {
+            throw new RuntimeException("Object cannot be null");
+        }
+
         return object;
     }
 
@@ -29,5 +38,19 @@ public abstract class AbstractMapService<T, ID> {
 
     void delete(T object) {
         this.map.entrySet().removeIf(entry -> entry.getValue().equals(object));
+    }
+
+    private Long getNextId() {
+
+        Long nextId = null;
+
+        try {
+            nextId = Collections.max(this.map.keySet()) + 1;
+        }
+        catch (NoSuchElementException e) {
+            nextId = 1L;
+        }
+
+        return nextId;
     }
 }
