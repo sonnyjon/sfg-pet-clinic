b454263f42444e58da3ffaa71b0c4779300457c4
sonnyjon <sonny.s.jon@gmail.com>
2022-05-30 08:53:04 -0700

    Issue #24: Create Pet Type, Pet, and Visit Entities

---
+++ pet-clinic-data/src/main/java/dev/sonnyjon/sfgpetclinic/model/Visit.java
@@ -0,0 +1,43 @@
+package dev.sonnyjon.sfgpetclinic.model;
+
+import java.time.LocalDate;
+
+/**
+ * Created by Sonny on 5/30/2022.
+ */
+public class Visit extends BaseEntity
+{
+    private LocalDate date;
+    private String description;
+    private Pet pet;
+
+    public LocalDate getDate()
+    {
+        return date;
+    }
+
+    public void setDate(LocalDate date)
+    {
+        this.date = date;
+    }
+
+    public String getDescription()
+    {
+        return description;
+    }
+
+    public void setDescription(String description)
+    {
+        this.description = description;
+    }
+
+    public Pet getPet()
+    {
+        return pet;
+    }
+
+    public void setPet(Pet pet)
+    {
+        this.pet = pet;
+    }
+}
