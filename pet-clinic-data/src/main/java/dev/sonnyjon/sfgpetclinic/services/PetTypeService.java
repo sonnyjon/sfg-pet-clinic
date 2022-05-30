5a6eefac8429c1ec433ee636a1fe840c3a3a0868
sonnyjon <sonny.s.jon@gmail.com>
2022-05-30 09:11:16 -0700

    Issue #27: Create PetType Map Service Impl

---
+++ pet-clinic-data/src/main/java/dev/sonnyjon/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,10 @@
+package dev.sonnyjon.sfgpetclinic.services;
+
+import dev.sonnyjon.sfgpetclinic.model.PetType;
+
+/**
+ * Created by Sonny on 5/30/2022.
+ */
+public interface PetTypeService extends CrudService<PetType, Long>
+{
+}
