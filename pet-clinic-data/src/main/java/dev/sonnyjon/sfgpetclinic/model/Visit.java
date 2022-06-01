b45028f335ab6e48a69b3931316e7966571c9cf1
sonnyjon <sonny.s.jon@gmail.com>
2022-06-01 10:17:23 -0700

    Issue #36: Create Visit Entity

--- pet-clinic-data/src/main/java/dev/sonnyjon/sfgpetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/dev/sonnyjon/sfgpetclinic/model/Visit.java
@@ -1,14 +1,23 @@
 package dev.sonnyjon.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
 /**
  * Created by Sonny on 5/30/2022.
  */
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity
 {
+    @Column(name = "date")
     private LocalDate date;
+
+    @Column(name = "description")
     private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
     private Pet pet;
 
     public LocalDate getDate()
