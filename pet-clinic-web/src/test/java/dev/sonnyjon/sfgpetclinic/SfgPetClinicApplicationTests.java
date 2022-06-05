97d9efa89c0aca560649538861041a4fca73f6a3
sonnyjon <sonny.s.jon@gmail.com>
2022-06-05 15:03:39 -0700

    Config to use JUnit 5

--- pet-clinic-web/src/test/java/dev/sonnyjon/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/dev/sonnyjon/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -1,11 +1,14 @@
 package dev.sonnyjon.sfgpetclinic;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
-class SfgPetClinicApplicationTests {
-
+class SfgPetClinicApplicationTests
+{
     @Test
     void contextLoads() {
     }
