c8041c21f36340555f83cc9945461139fb674c40
sonnyjon <sonny.s.jon@gmail.com>
2022-06-13 17:19:31 -0700

    Added a WebDataBinder to disallow the binding to the ID property

--- pet-clinic-web/src/main/java/dev/sonnyjon/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/dev/sonnyjon/sfgpetclinic/controllers/OwnerController.java
@@ -3,7 +3,9 @@ package dev.sonnyjon.sfgpetclinic.controllers;
 import dev.sonnyjon.sfgpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -20,6 +22,12 @@ public class OwnerController
     public OwnerController(OwnerService ownerService)
     {
         this.ownerService = ownerService;
+    }
+
+    @InitBinder
+    public void setAllowedFields(WebDataBinder dataBinder)
+    {
+        dataBinder.setDisallowedFields("id");
     }
 
     @GetMapping({"", "/", "/index", "/index.html"})
